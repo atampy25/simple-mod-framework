9bafe6360b2700d69696607526763d5c2d27ea43
Atampy26 <24306974+hitman-resources@users.noreply.github.com>
2021-10-06 18:26:30 +1000

    Add GUI mod manager

---
+++ Load Order Manager/src/mod.js
@@ -0,0 +1,298 @@
+var electron = require("electron")
+var Swal = require("sweetalert2")
+var fs = require('fs-extra')
+var path = require('path');
+var child_process = require("child_process");
+var AdmZip = require('adm-zip')
+
+window.$ = window.jQuery = require('jquery');
+
+async function execute() {
+	// if (!config.hasUsedGUI) {
+	// 	await swal.fire({
+	// 		title: "Select your game folder",
+	// 		html: "Click Continue to select your HITMAN 3 folder (the folder with Retail, Runtime and Launcher.exe inside it).",
+	// 		showConfirmButton: true,
+	// 		confirmButtonText: `Continue`
+	// 	})
+
+	// 	var gameDirectory = electron.remote.dialog.showOpenDialogSync({
+	// 		title: "Select your HITMAN 3 folder",
+	// 		buttonLabel: "Select",
+	// 		properties: ["openDirectory", "dontAddToRecent"]
+	// 	})[0]
+
+	// 	while (!fs.existsSync(path.join(gameDirectory, 'Runtime')) || !fs.existsSync(path.join(gameDirectory, 'Retail')) || !fs.existsSync(path.join(gameDirectory, 'ent')) || !fs.existsSync(path.join(gameDirectory, 'Launcher.exe'))) {
+	// 		await swal.fire({
+	// 			title: "Invalid folder",
+	// 			html: "Please select the HITMAN 3 folder - the folder containing ent, Retail, Runtime and Launcher.exe.",
+	// 			showConfirmButton: true,
+	// 			icon: "error",
+	// 			confirmButtonText: `Continue`
+	// 		})
+	
+	// 		var gameDirectory = electron.remote.dialog.showOpenDialogSync({
+	// 			title: "Select your HITMAN 3 folder, containing ent, Retail, Runtime and Launcher.exe.",
+	// 			buttonLabel: "Select",
+	// 			properties: ["openDirectory", "dontAddToRecent"]
+	// 		})[0]
+	// 	}
+
+	// 	var config = JSON.parse(fs.readFileSync("../config.json"))
+
+	// 	config.hasUsedGUI = true
+	// 	config.runtimePath = path.join(gameDirectory, 'Runtime')
+
+	// 	fs.writeFileSync("../config.json", JSON.stringify(config))
+
+	// 	showMessage("All set!", "You can begin using the Simple Mod Framework.", "success")
+	// }
+
+	await refreshMods()
+}
+
+async function refreshMods() {
+	$("#mods")[0].style.display = "flex"
+	$("#noModsMessage")[0].style.display = "none"
+	$("#enabledMods")[0].innerHTML = ""
+	$("#availableMods")[0].innerHTML = ""
+
+	var config = JSON.parse(fs.readFileSync("../config.json"))
+	
+	if (fs.readdirSync("../Mods").length > 0) {
+		for (modFolder of config.loadOrder) {
+			if (fs.existsSync(path.join("..", "Mods", modFolder, "manifest.json"))) {
+				var modManifest = JSON.parse(fs.readFileSync(path.join("..", "Mods", modFolder, "manifest.json")))
+				$("#enabledMods")[0].innerHTML += `<div class="p-12 bg-gray-900 w-full shadow-2xl rounded-md text-white">
+														<div class="mb-2">
+															<h3 class="font-semibold text-2xl inline"><img src="frameworkMod.png" class="w-10 inline align-middle"> <span class="align-middle">${modManifest.name}</span></h3><br>
+														</div>
+														<div class="mb-1">
+															<p>${modManifest.description}</p><br>
+														</div>
+														<neo-button label="Disable" gradientFrom="rose-400" gradientTo="red-500" onclick="disableMod('${modFolder}')" style="display: inline">
+															<i class="fas fa-times" slot="icon"></i>
+														</neo-button>
+														<neo-button label="Move" gradientFrom="fuchsia-400" gradientTo="violet-400" onclick="moveMod('${modFolder}')" style="display: inline">
+															<i class="fas fa-arrows-alt" slot="icon"></i>
+														</neo-button>
+													</div><br>`
+			} else {
+				$("#enabledMods")[0].innerHTML += `<div class="p-12 bg-gray-900 w-full shadow-2xl rounded-md text-white">
+														<div class="mb-2">
+															<h3 class="font-semibold text-2xl inline"><img src="rpkgMod.png" class="w-10 inline align-middle"> <span class="align-middle">${modFolder}</span></h3><br>
+														</div>
+														<div class="mb-1">
+															<p>RPKG-only mod</p><br>
+														</div>
+														<neo-button label="Disable" gradientFrom="rose-400" gradientTo="red-500" onclick="disableMod('${modFolder}')" style="display: inline">
+															<i class="fas fa-times" slot="icon"></i>
+														</neo-button>
+														<neo-button label="Move" gradientFrom="fuchsia-400" gradientTo="violet-400" onclick="moveMod('${modFolder}')" style="display: inline">
+															<i class="fas fa-arrows-alt" slot="icon"></i>
+														</neo-button>
+													</div><br>`
+			}
+		}
+		
+		for (modFolder of fs.readdirSync("../Mods").filter(folder => !config.loadOrder.includes(folder))) {
+			if (fs.existsSync(path.join("..", "Mods", modFolder, "manifest.json"))) {
+				var modManifest = JSON.parse(fs.readFileSync(path.join("..", "Mods", modFolder, "manifest.json")))
+				$("#availableMods")[0].innerHTML += `<div class="p-12 bg-gray-900 w-full shadow-2xl rounded-md text-white">
+														<div class="mb-2">
+															<h3 class="font-semibold text-2xl inline"><img src="frameworkMod.png" class="w-10 inline align-middle"> <span class="align-middle">${modManifest.name}</span></h3><br>
+														</div>
+														<div class="mb-1">
+															<p>${modManifest.description}</p><br>
+														</div>
+														<neo-button label="Enable" gradientFrom="emerald-400" gradientTo="lime-600" onclick="enableMod('${modFolder}')" style="display: inline">
+															<i class="fas fa-plus" slot="icon"></i>
+														</neo-button>
+													</div><br>`
+			} else {
+				$("#availableMods")[0].innerHTML += `<div class="p-12 bg-gray-900 w-full shadow-2xl rounded-md text-white">
+														<div class="mb-2">
+															<h3 class="font-semibold text-2xl inline"><img src="rpkgMod.png" class="w-10 inline align-middle"> <span class="align-middle">${modFolder}</span></h3><br>
+														</div>
+														<div class="mb-1">
+															<p>RPKG-only mod</p><br>
+														</div>
+														<neo-button label="Enable" gradientFrom="emerald-400" gradientTo="lime-600" onclick="enableMod('${modFolder}')" style="display: inline">
+															<i class="fas fa-plus" slot="icon"></i>
+														</neo-button>
+													</div><br>`
+			}
+		}
+	} else {
+		$("#noModsMessage")[0].style.display = "block"
+		$("#mods")[0].style.display = "none"
+	}
+}
+
+function showMessage(title, message, icon) {
+	Swal.fire({
+		showConfirmButton: false,
+		allowEnterKey: false,
+		title: title,
+		html: message,
+		icon: icon,
+		timer: 6000,
+		timerProgressBar: true
+	})
+}
+
+async function enableMod(mod) {
+	var config = JSON.parse(fs.readFileSync("../config.json"))
+
+	config.loadOrder.push(mod)
+
+	fs.writeFileSync("../config.json", JSON.stringify(config))
+
+	await refreshMods()
+}
+
+async function disableMod(mod) {
+	var config = JSON.parse(fs.readFileSync("../config.json"))
+
+	config.loadOrder = config.loadOrder.filter(a => a != mod)
+
+	fs.writeFileSync("../config.json", JSON.stringify(config))
+
+	await refreshMods()
+}
+
+async function moveMod(modID) {
+	var config = JSON.parse(fs.readFileSync("../config.json"))
+
+	var index = (await Swal.fire({
+		title: "Move Objective",
+		text: "Position in the list to move to (first item is 1, second is 2, etc.):",
+		input: "text",
+		inputAttributes: {
+		  autocapitalize: 'off'
+		},
+		showCancelButton: true,
+		confirmButtonText: 'OK',
+		allowOutsideClick: false
+	})).value
+
+	if (index !== null && index != "" && String(parseInt(index) - 1) != "NaN" && parseInt(index) - 1 >= 0) {
+		var modIndex = 0
+		for (mod of config.loadOrder) {
+			if (mod == modID) { break }
+			modIndex += 1
+		}
+
+		var removed = config.loadOrder.splice(modIndex, 1)[0]
+		config.loadOrder.splice(parseInt(index) - 1, 0, removed)
+	}
+	
+	fs.writeFileSync("../config.json", JSON.stringify(config))
+
+	await refreshMods()
+}
+
+async function deployMods() {
+	child_process.execSync("../Deploy.exe", {
+		cwd: '..'
+	})
+	showMessage("Deployed successfully", "Your mods have been deployed.", "success")
+}
+
+async function importZip() {
+	var modPath = electron.remote.dialog.showOpenDialogSync({
+		title: "Import a Framework ZIP file",
+		buttonLabel: "Import",
+		filters: [{ name: 'Framework ZIP Files', extensions: ['framework.zip'] }],
+		properties: ["openFile", "dontAddToRecent"]
+	})[0]
+
+	try {
+		fs.readdirSync("./staging").forEach(function(filePath) {
+			fs.unlinkSync(path.join("./staging", filePath))
+		})
+	} catch {}
+
+	if (!fs.existsSync('./staging')) { fs.mkdirSync('./staging') }
+
+	new AdmZip(modPath).extractAllTo("./staging")
+
+	fs.copySync("./staging", "../Mods")
+}
+
+async function importZIP() {
+	var modPath = electron.remote.dialog.showOpenDialogSync({
+		title: "Import a Framework ZIP file",
+		buttonLabel: "Import",
+		filters: [{ name: 'Framework ZIP Files', extensions: ['framework.zip'] }],
+		properties: ["openFile", "dontAddToRecent"]
+	})[0]
+
+	try {
+		fs.readdirSync("./staging").forEach(function(filePath) {
+			fs.unlinkSync(path.join("./staging", filePath))
+		})
+	} catch {}
+
+	if (!fs.existsSync('./staging')) { fs.mkdirSync('./staging') }
+
+	new AdmZip(modPath).extractAllTo("./staging")
+
+	fs.copySync("./staging", "../Mods")
+
+	refreshMods()
+}
+
+async function importRPKG() {
+	var modPath = electron.remote.dialog.showOpenDialogSync({
+		title: "Import an RPKG file",
+		buttonLabel: "Import",
+		filters: [{ name: 'RPKG Files', extensions: ['rpkg'] }],
+		properties: ["openFile", "dontAddToRecent"]
+	})[0]
+
+	var name = (await Swal.fire({
+		title: "Mod Name",
+		text: "Enter a short name for the mod (no special characters other than spaces):",
+		input: "text",
+		inputAttributes: {
+		  autocapitalize: 'off'
+		},
+		showCancelButton: true,
+		confirmButtonText: 'OK',
+		allowOutsideClick: false
+	})).value
+
+	if (!name) {return}
+
+	try {
+		var result = [...modPath.matchAll(/(chunk[0-9]*(?:patch[0-9]*)?)\.rpkg/g)]
+		result = [...result[result.length - 1][result[result.length - 1].length - 1].matchAll(/(chunk[0-9]*)/g)]
+		var chunk = result[result.length - 1][result[result.length - 1].length - 1]
+
+		if (!chunk) {
+			throw new Error()
+		}
+	} catch {
+		var chunk = (await Swal.fire({
+			title: "Mod Chunk",
+			text: "Enter the mod's chunk (if it advises you to name it chunk0patch2, for example, then it's chunk0):",
+			input: "text",
+			inputAttributes: {
+			  autocapitalize: 'off'
+			},
+			showCancelButton: true,
+			confirmButtonText: 'OK',
+			allowOutsideClick: false
+		})).value
+
+		if (!chunk) {return}
+	}
+
+	fs.ensureDirSync(path.join("..", "Mods", name, chunk))
+	fs.copyFileSync(modPath, path.join("..", "Mods", name, chunk, path.basename(modPath)))
+
+	await refreshMods()
+}
+
+execute()
---
+++ Load Order Manager/src/neo-ui.js
@@ -0,0 +1,272 @@
+customElements.define('neo-input', class AppDrawer extends HTMLElement {
+	static get observedAttributes() {
+		return ['label', 'placeholder']
+	}
+
+	get value() {
+		return this.shadowDOM.querySelector("input").value
+	}
+
+	set value(val) {
+		this.shadowDOM.querySelector("input").value = val
+	}
+
+	get disabled() {
+		return this.shadowDOM.querySelector("input").disabled
+	}
+
+	set disabled(val) {
+		this.shadowDOM.querySelector("input").disabled = val
+	}
+
+	get inputElement() {
+		return this.shadowDOM.querySelector("input")
+	}
+
+	constructor() {
+		super();
+
+		this.shadowDOM = this.attachShadow({mode: 'open'})
+		this.shadowDOM.innerHTML = `
+			<link rel="stylesheet" href="tailwind.min.css">
+			<style>
+				:host {
+					display: block;
+				}
+			</style>
+			<div class="mb-4">
+				<label class="block mb-2">
+					Input
+				</label>
+				<div>
+					<input class="shadow appearance-none border rounded w-full py-2 px-2.5 text-black" type="text" placeholder="Enter an input">
+				</div>
+			</div>
+		`
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		this.shadowDOM.querySelector("label").innerText = this.getAttribute("label")
+		this.shadowDOM.querySelector("input").placeholder = this.getAttribute("placeholder")
+	}
+});
+
+customElements.define('neo-checkbox', class AppDrawer extends HTMLElement {
+	static get observedAttributes() {
+		return ['label']
+	}
+
+	get checked() {
+		return this.shadowDOM.querySelector("input").checked
+	}
+
+	set checked(val) {
+		this.shadowDOM.querySelector("input").checked = val
+	}
+
+	get disabled() {
+		return this.shadowDOM.querySelector("input").disabled
+	}
+
+	set disabled(val) {
+		this.shadowDOM.querySelector("input").disabled = val
+	}
+
+	get inputElement() {
+		return this.shadowDOM.querySelector("input")
+	}
+
+	constructor() {
+		super();
+
+		this.shadowDOM = this.attachShadow({mode: 'open'})
+		this.shadowDOM.innerHTML = `
+			<link rel="stylesheet" href="tailwind.min.css">
+			<style>
+				:host {
+					display: block;
+				}
+			</style>
+			<label class="inline-flex items-center mb-3">
+				<input type="checkbox" class="form-checkbox cursor-pointer h-5 w-5 text-gray-700 bg-white"><span class="ml-2"></span>
+			</label>
+		`
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		this.shadowDOM.querySelector("span").innerText = this.getAttribute("label")
+	}
+});
+
+customElements.define('neo-select', class AppDrawer extends HTMLElement {
+	static get observedAttributes() {
+		return ['label']
+	}
+
+	get value() {
+		return this.shadowDOM.querySelector("select").value
+	}
+
+	set value(val) {
+		this.shadowDOM.querySelector("select").value = val
+	}
+
+	get disabled() {
+		return this.shadowDOM.querySelector("select").disabled
+	}
+
+	set disabled(val) {
+		this.shadowDOM.querySelector("select").disabled = val
+	}
+
+	get selectElement() {
+		return this.shadowDOM.querySelector("select")
+	}
+
+	constructor() {
+		super();
+
+		this.shadowDOM = this.attachShadow({mode: 'open'})
+		this.shadowDOM.innerHTML = `
+			<link rel="stylesheet" href="tailwind.min.css">
+			<style>
+				:host {
+					display: block;
+				}
+			</style>
+			<div class="relative">
+				<label class="block mb-2">
+					Select an option
+				</label>
+				<select class="w-full shadow border text-black py-2 px-3 pr-8 rounded appearance-none" style="background-image: linear-gradient(45deg, transparent 50%, gray 50%), linear-gradient(135deg, gray 50%, transparent 50%), linear-gradient(to right, #ccc, #ccc); background-position: calc(100% - 20px) calc(1em + 2px), calc(100% - 15px) calc(1em + 2px), calc(100% - 2.5em) 0.5em; background-size: 5px 5px, 5px 5px, 1px 1.5em; background-repeat: no-repeat;"></select>
+				<slot></slot>
+			</div>
+		`
+
+		this.shadowDOM.addEventListener('slotchange', event => {
+            if (this.querySelector('option')) {
+				this.shadowDOM.querySelector("select").append(this.querySelector('option'))
+			}
+        })
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		this.shadowDOM.querySelector("label").innerText = this.getAttribute("label")
+	}
+});
+
+customElements.define('neo-file-upload', class AppDrawer extends HTMLElement {
+	static get observedAttributes() {
+		return ['label']
+	}
+
+	get value() {
+		return this.shadowDOM.querySelector("input").value
+	}
+
+	set value(val) {
+		this.shadowDOM.querySelector("input").value = val
+	}
+
+	get disabled() {
+		return this.shadowDOM.querySelector("input").disabled
+	}
+
+	set disabled(val) {
+		this.shadowDOM.querySelector("input").disabled = val
+	}
+
+	get inputElement() {
+		return this.shadowDOM.querySelector("input")
+	}
+
+	get uploadedFilePath() {
+		return this.shadowDOM.getElementById("fileUpload").files[0]
+	}
+
+	updateFileName() {
+		var fileInput = this.shadowDOM.getElementById("fileUpload")
+		if (fileInput.files.length > 0 && (!this.acceptedFileTypes || this.acceptedFileTypes.includes(fileInput.files[0].type))) {
+			this.shadowDOM.getElementById("fileName").textContent = fileInput.files[0].name
+		} else {
+			fileInput.value = ""
+			this.shadowDOM.getElementById("fileName").textContent = "Must be " + this.acceptedFileTypes.join(" or ")
+		}
+	}
+
+	constructor() {
+		super();
+
+		this.acceptedFileTypes = this.getAttribute("acceptedFileTypes") ? this.getAttribute("acceptedFileTypes").split(", ") : false
+
+		this.shadowDOM = this.attachShadow({mode: 'open'})
+		this.shadowDOM.innerHTML = `
+			<link rel="stylesheet" href="tailwind.min.css">
+			<style>
+				:host {
+					display: block;
+					margin-top: 2.5rem;
+					margin-bottom: 2.5rem;
+				}
+			</style>
+			<label class="w-64 px-4 py-6 text-blue rounded-lg shadow-lg tracking-wide border border-blue cursor-pointer hover:bg-blue hover:text-white">
+				<span class="mt-2"><slot name="icon"></slot> <span id="label" class="font-semibold"></span></span>
+				<span class="mt-2 font-light" id="fileName">No file uploaded</span>
+				<input type='file' class="hidden" id="fileUpload" onchange="this.getRootNode().host.updateFileName()"/>
+			</label>
+		`
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		this.shadowDOM.querySelector("label").innerText = this.getAttribute("label")
+	}
+});
+
+customElements.define('neo-button', class AppDrawer extends HTMLElement {
+	static get observedAttributes() {
+		return ['label', 'gradientFrom', 'gradientTo']
+	}
+
+	get value() {
+		return this.shadowDOM.querySelector("button").value
+	}
+
+	set value(val) {
+		this.shadowDOM.querySelector("button").value = val
+	}
+
+	get disabled() {
+		return this.shadowDOM.querySelector("button").disabled
+	}
+
+	set disabled(val) {
+		this.shadowDOM.querySelector("button").disabled = val
+	}
+
+	get buttonElement() {
+		return this.shadowDOM.querySelector("button")
+	}
+
+	constructor() {
+		super();
+
+		this.shadowDOM = this.attachShadow({mode: 'open'})
+		this.shadowDOM.innerHTML = `
+			<link rel="stylesheet" href="tailwind.min.css">
+			<style>
+				:host {
+					display: block;
+				}
+			</style>
+			<button type="button" class="bg-gradient-to-r text-white font-light px-3.5 py-3 rounded-md m-2" style="font-family: 'Fira Code'">
+				<slot name="icon"></slot> <span></span>
+			</button>
+		`
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		this.shadowDOM.querySelector("span").innerText = this.getAttribute("label")
+		this.shadowDOM.querySelector("button").classList.add("from-" + this.getAttribute("gradientFrom"))
+		this.shadowDOM.querySelector("button").classList.add("to-" + this.getAttribute("gradientTo"))
+	}
+});
