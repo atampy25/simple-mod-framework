29c4cb2d9b83b9b331f18d626f40b1371d2e8e86
Atampy26 <24306974+hitman-resources@users.noreply.github.com>
2021-10-06 20:28:50 +1000

    ä

--- Load Order Manager/src/index.js
+++ Load Order Manager/src/index.js
@@ -14,6 +14,7 @@ const createWindow = () => {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
+      contextIsolation: false,
       enableRemoteModule: true
     }
   });
