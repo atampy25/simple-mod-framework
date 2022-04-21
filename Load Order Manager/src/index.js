c37e3d2187951c78d97cc2793a05a25e859541cc
Reece Dunham <me@rdil.rocks>
2022-04-21 14:27:06 -0400

    What

--- Load Order Manager/src/index.js
+++ Load Order Manager/src/index.js
@@ -1,8 +1,9 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const electronSquirrel = require('electron-squirrel-startup');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+if (electronSquirrel) {
   app.quit();
 }
 
@@ -49,7 +50,7 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
+  // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
