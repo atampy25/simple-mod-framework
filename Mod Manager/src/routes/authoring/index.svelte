e6c5723ad16f84650f0461d18bc712c151f1ecb6
atampy25 <24306974+atampy25@users.noreply.github.com>
2022-10-30 15:14:40 +1000

    fix(GUI): truncate mod authoring text

--- Mod Manager/src/routes/authoring/index.svelte
+++ Mod Manager/src/routes/authoring/index.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { scale } from "svelte/transition"
 
-	import { Button, Tile } from "carbon-components-svelte"
+	import { Button, Tile, Truncate } from "carbon-components-svelte"
 	import { getAllMods, getManifestFromModID, modIsFramework } from "$lib/utils"
 	import Edit from "carbon-icons-svelte/lib/Edit.svelte"
 	import { goto } from "$app/navigation"
@@ -24,12 +24,14 @@
 			.map((a) => getManifestFromModID(a)) as manifest (manifest.id)}
 			<Tile>
 				<div class="flex flex-row items-center gap-8">
-					<div class="flex-grow">
+					<div class="flex-grow" style="max-width: 28rem">
 						<span class="text-xs">{manifest.authors.length == 1 ? manifest.authors.join(", ") : manifest.authors.slice(0, -1).join(", ") + " and " + manifest.authors.at(-1)}</span>
 						<h4 class="mb-1 overflow-x-auto w-full">
 							{manifest.name}
 						</h4>
-						{manifest.description}
+						<Truncate>
+							{manifest.description}
+						</Truncate>
 					</div>
 					<div class="flex-shrink-0">
 						<Button kind="ghost" icon={Edit} iconDescription="Edit this mod" on:click={() => goto(`/authoring/${manifest.id}`)} />
