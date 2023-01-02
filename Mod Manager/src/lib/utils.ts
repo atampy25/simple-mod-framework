f4d21b859fdf1cedf0ec8fbd88405434fc99a011
atampy25 <24306974+atampy25@users.noreply.github.com>
2023-01-02 13:50:34 +1000

    chore: bump version to 2.9.1

--- Mod Manager/src/lib/utils.ts
+++ Mod Manager/src/lib/utils.ts
@@ -8,7 +8,7 @@ import manifestSchema from "$lib/manifest-schema.json"
 import memoize from "lodash.memoize"
 import merge from "lodash.mergewith"
 
-export const FrameworkVersion = "2.9.0"
+export const FrameworkVersion = "2.9.1"
 
 const validateManifest = new Ajv().compile(manifestSchema)
 
--- src/core.ts
+++ src/core.ts
@@ -1,4 +1,4 @@
-const FrameworkVersion = "2.9.0"
+const FrameworkVersion = "2.9.1"
 const isDevBuild = false
 
 import * as Sentry from "@sentry/node"
