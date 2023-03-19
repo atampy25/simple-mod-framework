cf36f07acdac8d1ecac2276b89aecd4f34c893bb
atampy25 <24306974+atampy25@users.noreply.github.com>
2023-03-19 10:47:08 +1000

    chore: bump version to 2.19.0

--- Mod Manager/src/lib/utils.ts
+++ Mod Manager/src/lib/utils.ts
@@ -10,7 +10,7 @@ import merge from "lodash.mergewith"
 import semver from "semver"
 import { cloneDeep } from "lodash"
 
-export const FrameworkVersion = "2.18.0"
+export const FrameworkVersion = "2.19.0"
 
 const validateManifest = new Ajv().compile(manifestSchema)
 
