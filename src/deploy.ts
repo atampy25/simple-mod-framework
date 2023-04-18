atampy25/simple-mod-framework

commit 63fd99926a7ddc2ea836fcc4d3e8b9350d96f957

perf(deploy): don't needlessly stage non-content files

src/deploy.ts
@@ -1510,12 +1510,14 @@ export default async function deploy(
 					break
 				}
 				default: // Copy the file to the staging directory; we don't cache these for obvious reasons
-					fs.writeFileSync(
-						content.source === "disk"
-							? path.join(process.cwd(), "staging", `chunk${content.chunk}`, path.basename(content.path))
-							: path.join(process.cwd(), "staging", `chunk${content.chunk}`, `${content.extraInformation.runtimeID!}.${content.extraInformation.fileType!}`),
-						content.source === "disk" ? fs.readFileSync(content.path) : Buffer.from(await content.content.arrayBuffer())
-					)
+					if ((content.source === "disk" ? path.basename(content.path).split(".").slice(1).join(".") : content.extraInformation.fileType!).length === 4) {
+						fs.writeFileSync(
+							content.source === "disk"
+								? path.join(process.cwd(), "staging", `chunk${content.chunk}`, path.basename(content.path))
+								: path.join(process.cwd(), "staging", `chunk${content.chunk}`, `${content.extraInformation.runtimeID!}.${content.extraInformation.fileType!}`),
+							content.source === "disk" ? fs.readFileSync(content.path) : Buffer.from(await content.content.arrayBuffer())
+						)
+					}
 					break
 			}
 

src/discover.ts
@@ -333,33 +333,35 @@ export default async function discover(): Promise<{ [x: string]: { hash: string;
 								affected.push(path.basename(contentFilePath).split(".")[0].split("~")[0])
 								break
 							default: // Replaces a file with a raw file
-								fileToReplace = path.basename(contentFilePath).split(".")[0]
+								if (path.basename(contentFilePath).split(".").slice(1).join(".").length === 4) {
+									fileToReplace = path.basename(contentFilePath).split(".")[0]
 
-								if (baseGameEntityHashes.has(fileToReplace)) {
-									await logger.warn(
-										`Mod ${manifest.name} replaces a base game entity file (${fileToReplace}) with a raw file. This can cause compatibility issues, it makes the mod harder to work with and it requires more work when the game updates. Mod developers can fix this easily by using an entity.patch.json file.`
-									)
+									if (baseGameEntityHashes.has(fileToReplace)) {
+										await logger.warn(
+											`Mod ${manifest.name} replaces a base game entity file (${fileToReplace}) with a raw file. This can cause compatibility issues, it makes the mod harder to work with and it requires more work when the game updates. Mod developers can fix this easily by using an entity.patch.json file.`
+										)
+									}
+
+									if (fileToReplace === "00204D1AFD76AB13") {
+										await logger.warn(
+											`Mod ${manifest.name} replaces the repository file (${fileToReplace}) in its entirety. This can cause compatibility issues, it makes the mod harder to work with and it requires more work when the game updates. Mod developers can fix this easily by using a repository.json or JSON.patch.json file.`
+										)
+									}
+
+									if (fileToReplace === "0057C2C3941115CA") {
+										await logger.warn(
+											`Mod ${manifest.name} replaces the unlockables file (${fileToReplace}) in its entirety. This can cause compatibility issues, it makes the mod harder to work with and it requires more work when the game updates. Mod developers can fix this easily by using an unlockables.json or JSON.patch.json file.`
+										)
+									}
+
+									if (path.basename(contentFilePath).split(".")[1] === "WWEV") {
+										await logger.warn(
+											`Mod ${manifest.name} replaces a sound bank file in its entirety. This can cause compatibility issues, it makes the mod harder to work with and it can require more work when the game updates. Mod developers can fix this easily by using an sfx.wem file.`
+										)
+									}
+
+									affected.push(fileToReplace)
 								}
-
-								if (fileToReplace === "00204D1AFD76AB13") {
-									await logger.warn(
-										`Mod ${manifest.name} replaces the repository file (${fileToReplace}) in its entirety. This can cause compatibility issues, it makes the mod harder to work with and it requires more work when the game updates. Mod developers can fix this easily by using a repository.json or JSON.patch.json file.`
-									)
-								}
-
-								if (fileToReplace === "0057C2C3941115CA") {
-									await logger.warn(
-										`Mod ${manifest.name} replaces the unlockables file (${fileToReplace}) in its entirety. This can cause compatibility issues, it makes the mod harder to work with and it requires more work when the game updates. Mod developers can fix this easily by using an unlockables.json or JSON.patch.json file.`
-									)
-								}
-
-								if (path.basename(contentFilePath).split(".")[1] === "WWEV") {
-									await logger.warn(
-										`Mod ${manifest.name} replaces a sound bank file in its entirety. This can cause compatibility issues, it makes the mod harder to work with and it can require more work when the game updates. Mod developers can fix this easily by using an sfx.wem file.`
-									)
-								}
-
-								affected.push(fileToReplace)
 								break
 						}
 

commit 1e1f754525faf4e37eed72c72b82b47c965e7511

fix(deploy): stage meta files
Fixes an issue introduced in 63fd999.

src/deploy.ts
@@ -1510,7 +1510,7 @@ export default async function deploy(
 					break
 				}
 				default: // Copy the file to the staging directory; we don't cache these for obvious reasons
-					if ((content.source === "disk" ? path.basename(content.path).split(".").slice(1).join(".") : content.extraInformation.fileType!).length === 4) {
+					if ((content.source === "disk" ? path.basename(content.path).split(".").slice(1).join(".") : content.extraInformation.fileType!).length === 4 || (content.source === "disk" ? path.basename(content.path).split(".").slice(1).join(".") : content.extraInformation.fileType!).endsWith("meta")) {
 						fs.writeFileSync(
 							content.source === "disk"
 								? path.join(process.cwd(), "staging", `chunk${content.chunk}`, path.basename(content.path))