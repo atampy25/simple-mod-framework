86c7c9dd4becb4457be3674bf3c53dfbdf181e51
atampy25 <24306974+atampy25@users.noreply.github.com>
2023-04-21 17:02:00 +1000

    chore: bump version to 2.22.1

--- Mod Manager/src/lib/utils.ts
+++ Mod Manager/src/lib/utils.ts
@@ -14,7 +14,7 @@ import memoize from "lodash.memoize"
 import merge from "lodash.mergewith"
 import semver from "semver"
 
-export const FrameworkVersion = "2.22.0"
+export const FrameworkVersion = "2.22.1"
 
 const validateManifest = new Ajv({ strict: false }).compile(manifestSchema)
 
