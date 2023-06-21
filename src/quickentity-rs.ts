75a39f4855b71b7bcc25978d70dc6738d9790e0b
atampy25 <24306974+atampy25@users.noreply.github.com>
2023-06-21 23:15:02 +1000

    perf!: faster JSON parsing, apply patches in-memory
    BREAKING CHANGE: Patches with versions prior QuickEntity 3.1 are no longer supported and will throw a deploy error.

--- src/quickentity-rs.ts
+++ src/quickentity-rs.ts
@@ -5,18 +5,36 @@ import { logger } from "./core-singleton"
 
 const execCommand = function (command: string) {
 	void logger.verbose(`Executing QN 3.1 command ${command}`)
-	child_process.execSync(command, { stdio: [ "pipe", "inherit", "inherit" ] })
+	child_process.execSync(command, { stdio: ["pipe", "inherit", "inherit"] })
 }
 
-export async function convert(game: string, TEMP: string, TEMPmeta: string, TBLU: string, TBLUmeta: string, output: string) {
+export async function convert(TEMP: string, TEMPmeta: string, TBLU: string, TBLUmeta: string, output: string) {
 	execCommand(
 		`"Third-Party\\quickentity-rs.exe" entity convert --input-factory "${TEMP}" --input-factory-meta "${TEMPmeta}" --input-blueprint "${TBLU}" --input-blueprint-meta "${TBLUmeta}" --output "${output}" --lossless`
 	)
 }
 
-export async function generate(game: string, input: string, TEMP: string, TEMPmeta: string, TBLU: string, TBLUmeta: string) {
+export async function generate(input: string, TEMP: string, TEMPmeta: string, TBLU: string, TBLUmeta: string) {
 	execCommand(
 		`"Third-Party\\quickentity-rs.exe" entity generate --input "${input}" --output-factory "${TEMP}" --output-factory-meta "${TEMPmeta}" --output-blueprint "${TBLU}" --output-blueprint-meta "${TBLUmeta}"`
+	)
+}
+
+export async function convertPatchGenerate(
+	inTEMP: string,
+	inTEMPmeta: string,
+	inTBLU: string,
+	inTBLUmeta: string,
+	patches: string[],
+	outTEMP: string,
+	outTEMPmeta: string,
+	outTBLU: string,
+	outTBLUmeta: string
+) {
+	execCommand(
+		`"Third-Party\\quickentity-rs.exe" convert-patch-generate --input-factory "${inTEMP}" --input-factory-meta "${inTEMPmeta}" --input-blueprint "${inTBLU}" --input-blueprint-meta "${inTBLUmeta}" --output-factory "${outTEMP}" --output-factory-meta "${outTEMPmeta}" --output-blueprint "${outTBLU}" --output-blueprint-meta "${outTBLUmeta}" --lossless --permissive --patches "${patches.join(
+			'";"'
+		)}"`
 	)
 }
 
