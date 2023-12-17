24aa7f5cf039b9fa0322bc51c37e793930a1d52f
atampy25 <24306974+atampy25@users.noreply.github.com>
2023-12-17 12:18:22 +1000

    chore: bump version to 2.33.7

--- Mod Manager/src/lib/utils.ts
+++ Mod Manager/src/lib/utils.ts
@@ -14,7 +14,7 @@ import memoize from "lodash.memoize"
 import merge from "lodash.mergewith"
 import semver from "semver"
 
-export const FrameworkVersion = "2.33.6"
+export const FrameworkVersion = "2.33.7"
 
 const validateManifest = new Ajv({ strict: false }).compile(manifestSchema)
 
--- src/core.ts
+++ src/core.ts
@@ -1,4 +1,4 @@
-const FrameworkVersion = "2.33.6"
+const FrameworkVersion = "2.33.7"
 const isDevBuild = false
 
 import * as Sentry from "@sentry/node"
