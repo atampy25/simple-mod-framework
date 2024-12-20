d8604cdb14d4cb4aba92c6e34bf4cb6f20802ac1
AnthonyFuller <anthony@hitmods.com>
2024-12-20 10:18:01 +0000

    fix(deploy): update for game version 3.210.2

--- src/main.ts
+++ src/main.ts
@@ -21,17 +21,17 @@ import { xxhash3 } from "hash-wasm"
 require("clarify")
 
 const gameHashes = {
-	"09278760d4943ad21d04921169366d54": Platform.epic, // base game
-	//"a8752bc4b36a74600549778685db3b4c": Platform.epic, // ansel unlock
+	"e11edfba6fd5d4121fa45b1c7a29376b": Platform.epic, // base game
+	"ca246beded6e1cff132aa4c847d9b29c": Platform.epic, // ansel unlock
 	//"09278760d4943ad21d04921169366d54": Platform.epic, // ansel no collision
 	//"a8752bc4b36a74600549778685db3b4c": Platform.epic, // ansel unlock + no collision
-	"20051caae42f6079bc1e397fbed4dda0": Platform.steam, // base game
-	//"b71da06044f81a81c40874cb9adc3dbe": Platform.steam, // ansel unlock
+	"a062b0d7a928476a70c5a3b34067544b": Platform.steam, // base game
+	"c4a2a4f33d5b8d2c160452c6e7ff502c": Platform.steam, // ansel unlock
 	//"28607baf7a75271b6924fe0d52263600": Platform.steam, // ansel no collision
 	//"d028074b654cb628ef88ced7b5d3eb96": Platform.steam, // ansel unlock + no collision
 
 	// Gamepass/store protects the EXE from reading so we can't hash it, instead we hash the game config
-	"ecba891673231b98cd80f5a490843365": Platform.microsoft
+	"44e3b8137b9269026b02404e282e08f3": Platform.microsoft
 } as {
 	[k: string]: Platform
 }
